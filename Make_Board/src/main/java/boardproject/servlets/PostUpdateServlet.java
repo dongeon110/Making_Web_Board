3b366a390eb0b540bc1bf621b0d05e289615943f
dongeon110 <dongeon110@gmail.com>
2022-09-06 17:44:31 +0900

    2209060 수정하기 비밀번호 확인 기능

--- Make_Board/src/main/java/boardproject/servlets/PostUpdateServlet.java
+++ Make_Board/src/main/java/boardproject/servlets/PostUpdateServlet.java
@@ -9,10 +9,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.RequestDispatcher;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import boardproject.vo.PostVO;
 import boardproject.dao.PostDao;
@@ -30,16 +26,14 @@ public class PostUpdateServlet extends HttpServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
+
 		// 아이디 확인
 		try {
 			ServletContext sc = this.getServletContext();
+			response.setContentType("text/html; charset=UTF-8");
 			
 			PostDao postDao = (PostDao) sc.getAttribute("postDao");
-			request.setAttribute("thisNo", (Integer)sc.getAttribute("thisNo"));
+			
 			
 			PostVO checkPostVO = new PostVO()
 					.setPostNo((Integer)sc.getAttribute("thisNo"))
@@ -47,20 +41,17 @@ public class PostUpdateServlet extends HttpServlet {
 			if (postDao.checkPw(checkPostVO)) {
 				request.setAttribute("postVO", postDao.selectOne(Integer.parseInt(request.getParameter("no"))));
 				RequestDispatcher rd = request.getRequestDispatcher("/board/postUpdate.jsp");
-				rd.include(request, response);
+
+				rd.forward(request, response);
+
 			} else {
 				RequestDispatcher rd = request.getRequestDispatcher("/auth/CheckPwFail.jsp");
 				rd.forward(request, response);
 			}
 			
 			
-			
 		} catch (Exception e) {
 			throw new ServletException(e);
-		} finally {
-			try {if (rs != null) rs.close();} catch (Exception e) {}
-			try {if (stmt != null) stmt.close();} catch (Exception e) {}
-			try {if (conn != null) stmt.close();} catch (Exception e) {}
-		}
+		} finally {}
 	}
 }
